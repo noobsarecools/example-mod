9fa5f9ae0120702c748dc97e5422137a9e0a92db
noobsarecools <amyratliff1972@gmail.com>
2026-07-14 18:29:06 +0000

    Added my layout list

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,37 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CreatorLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class $modify(MyLayoutMod, CreatorLayer) {
+    bool init() {
+        // 1. Load the normal Creator Layer first
+        if (!CreatorLayer::init()) return false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+        // 2. Find the menu grid where game buttons live
+        auto menu = this->getChildByID("creator-buttons-menu");
+        
+        // 3. Create your custom layout button (Uses the Blue Spark sprite frame)
+        auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_downloadsBtn_001.png");
+        auto myButton = CCMenuItemSpriteExtra::create(
+            btnSprite, 
+            this, 
+            menu_selector(MyLayoutMod::onOpenLayouts)
+        );
+        
+        // 4. Attach the button to the menu and space everything nicely
+        menu->addChild(myButton);
+        menu->updateLayout();
+        return true;
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    // 5. When clicked, load your custom list of layout IDs right into GD!
+    void onOpenLayouts(CCObject* sender) {
+        // --- PUT YOUR RATED LEVEL IDS IN THE QUOTES BELOW SEPARATED BY COMMAS ---
+        std::string myRatedLevels = "10234567,9876543,55443322"; 
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        auto searchObject = GameLevelManager::sharedState()->getSearchObject(SearchType::Custom, myRatedLevels);
+        auto searchLayer = LevelSearchLayer::create(searchObject);
+        CCDirector::sharedDirector()->getRunningScene()->addChild(searchLayer);
+    }
 };
